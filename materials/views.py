5f0bf900bed1d148fc530b83ef748902bb38832a
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-11-10 22:40:39 +0300

    fixes for materials

--- materials/views.py
+++ materials/views.py
@@ -5,15 +5,21 @@ from rest_framework import generics
 
 
 class CourseViewSet(ModelViewSet):
+    """ViewSet для работы с моделью Course."""
+
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
+    """Представление для получения списка уроков и создания нового урока"""
+
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
 
 class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """Представление для получения, изменения и удаления конкретного урока"""
+
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
