4017e9d90449b0f6d1c9f9a2a01c15b2645668cf
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-11-10 22:40:15 +0300

    fixes for config

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/', include('materials.urls')),
-    path('api/', include('users.urls')),
+    path('api/', include('materials.urls', namespace="materials")),
+    path('api/', include('users.urls', namespace="users")),
 ]
