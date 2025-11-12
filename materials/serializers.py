674d3aebaa47ccbc7073b136dd6ee6767b85f659
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-11-12 23:31:54 +0300

    fixes users, materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
+
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Course"""
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Lesson"""
+    """Сериализатор модели Lesson"""
 
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Course с уроками и их количеством"""
+
+    lessons = LessonSerializer(many=True, read_only=True)
+    lesson_count = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ["id", "name", "preview", "description", "lesson_count", "lessons"]
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
