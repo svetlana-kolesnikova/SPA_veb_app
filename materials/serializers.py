f11f5493a895ebc05bcb24302cd12760205e1910
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-11-14 22:53:54 +0300

    fixes for materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,3 +1,4 @@
+# materials/serializers.py
 from rest_framework import serializers
 
 from .models import Course, Lesson
@@ -6,9 +7,19 @@ from .models import Course, Lesson
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор модели Lesson"""
 
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Lesson
-        fields = "__all__"
+        fields = ["id", "name", "description", "preview", "video_link", "course", "owner"]
+
+    def create(self, validated_data):
+        """
+        Автоматически назначаем владельца урока
+        """
+        request = self.context.get("request")
+        validated_data["owner"] = request.user
+        return super().create(validated_data)
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -16,10 +27,19 @@ class CourseSerializer(serializers.ModelSerializer):
 
     lessons = LessonSerializer(many=True, read_only=True)
     lesson_count = serializers.SerializerMethodField(read_only=True)
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Course
-        fields = ["id", "name", "preview", "description", "lesson_count", "lessons"]
+        fields = ["id", "name", "preview", "description", "lesson_count", "lessons", "owner"]
 
     def get_lesson_count(self, obj):
         return obj.lessons.count()
+
+    def create(self, validated_data):
+        """
+        Автоматически назначаем владельца курса
+        """
+        request = self.context.get("request")
+        validated_data["owner"] = request.user
+        return super().create(validated_data)
--- materials/urls.py
+++ materials/urls.py
@@ -1,3 +1,4 @@
+# materials/urls.py
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -6,10 +7,10 @@ from .views import CourseViewSet, LessonListCreateAPIView, LessonRetrieveUpdateD
 app_name = "materials"
 
 router = DefaultRouter()
-router.register("courses", CourseViewSet)
+router.register("courses", CourseViewSet, basename="course")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("lessons/", LessonListCreateAPIView.as_view()),
-    path("lessons/<int:pk>/", LessonRetrieveUpdateDestroyAPIView.as_view()),
+    path("lessons/", LessonListCreateAPIView.as_view(), name="lesson-list-create"),
+    path("lessons/<int:pk>/", LessonRetrieveUpdateDestroyAPIView.as_view(), name="lesson-detail"),
 ]
