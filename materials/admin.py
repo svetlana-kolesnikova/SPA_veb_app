a4f6e1b25613ed64b7ef03fefc08d1aa715a8877
svetlana-kolesnikova <sv.ps1286@gmail.com>
2026-01-22 21:20:26 +0300

    fixes

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,4 @@
+# materials/admin.py
 from django.contrib import admin
 
 from .models import Course, Lesson
--- materials/tasks.py
+++ materials/tasks.py
@@ -1,3 +1,4 @@
+# materials/tasks.py
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
